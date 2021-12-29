5e11115324051eb10a7151f2a4bb263be9d60c6b
p0lunin <dmytro.polunin@gmail.com>
2021-12-29 18:59:50 +0200

    added State<> struct

--- src/lib.rs
+++ src/lib.rs
@@ -7,49 +7,79 @@ use downcast_rs::impl_downcast;
 pub trait Vertex: Downcast {
     fn entry(&self);
     fn exit(&self);
-    fn data(self: Box<Self>) -> Box<dyn Any>;
+    fn get_data(&mut self) -> Box<dyn Any>;
+    fn set_data(&mut self, data: Box<dyn Any>);
     fn data_tid(&self) -> TypeId { self.as_any().type_id() }
 }
 impl_downcast!(Vertex);
 
 pub struct InitialPseudostate;
 
-impl Vertex for InitialPseudostate {
-    fn entry(&self) {}
-    fn exit(&self) {}
-    fn data(self: Box<Self>) -> Box<dyn Any> { self }
+struct State<T, Entry, Exit> {
+    data: Option<Box<T>>,
+    entry: Entry,
+    exit: Exit,
 }
 
-#[repr(transparent)]
-struct State<T>(T);
+fn do_nothing<T>(_: &T) {}
 
-impl<T: Any> Vertex for State<T> {
-    fn entry(&self) {}
-    fn exit(&self) {}
-    fn data(self: Box<Self>) -> Box<dyn Any> {
-        unsafe {
-            // SAFETY: self if #[repr(transparent)]
-            let b: Box<T> = std::mem::transmute(self);
-            b
-        }
+impl<T> State<T, (), ()> {
+    pub fn new(data: T) -> State<T, impl Fn(&T), impl Fn(&T)> {
+        State { data: Some(Box::new(data)), entry: do_nothing, exit: do_nothing }
+    }
+
+    pub fn empty<T1>() -> State<T1, impl Fn(&T), impl Fn(&T)> {
+        State { data: None, entry: do_nothing, exit: do_nothing }
     }
 }
 
-impl InitialPseudostate {
-    fn boxed() -> Box<dyn Vertex> {
-        Box::new(Self)
+impl<T, Entry1, Exit> State<T, Entry1, Exit> {
+    pub fn entry<Entry>(self, entry: Entry) -> State<T, Entry, Exit> {
+        let State { data, exit, .. } = self;
+        State { entry, data, exit }
     }
+}
+
+impl<T, Entry, Exit1> State<T, Entry, Exit1> {
+    pub fn exit<Exit>(self, exit: Exit) -> State<T, Entry, Exit> {
+        let State { data, entry, .. } = self;
+        State { exit, data, entry }
+    }
+}
+
+impl<T, Entry, Exit1> State<T, Entry, Exit1>
+where
+    Self: Vertex,
+{
+    pub fn boxed(self) -> Box<dyn Vertex> {
+        Box::new(self)
+    }
+}
+
+impl<T: Any, Entry, Exit> Vertex for State<T, Entry, Exit>
+where
+    Entry: Fn(&T) + 'static,
+    Exit: Fn(&T) + 'static,
+{
+    fn entry(&self) { (self.entry)(&self.data.as_ref().expect("It must guaranteed by the caller").as_ref()); }
+    fn exit(&self) { (self.exit)(&self.data.as_ref().expect("It must guaranteed by the caller").as_ref()); }
+    fn get_data(&mut self) -> Box<dyn Any> {
+        self.data.take().expect("This method must be called only once.")
+    }
+    fn set_data(&mut self, data: Box<dyn Any>) {
+        self.data = Some(data.downcast().expect("It must guaranteed by the caller"))
+    }
+    fn data_tid(&self) -> TypeId { TypeId::of::<T>() }
 }
 
 pub trait Transition<Event> {
     type Answer;
-    fn transition(&self, from: Box<dyn Vertex>, event: Event) -> Result<(Box<dyn Vertex>, Self::Answer), TransitionError<Event>>;
+    fn transition(&self, from: &mut dyn Vertex, event: Event) -> Result<(Box<dyn Any>, Self::Answer), TransitionError<Event>>;
     fn input_tid(&self) -> TypeId;
     fn output_tid(&self) -> TypeId;
 }
 
 pub struct TransitionError<Event> {
-    from: Box<dyn Vertex>,
     event: Event,
     kind: TransitionErrorKind,
 }
@@ -67,8 +97,8 @@ fn ftrans<F: Into<FuncTransition<F, Args>>, Args>(f: F) -> FuncTransition<F, Arg
 impl<F, Input, Output, Event, Answer> From<F> for FuncTransition<F, (Input, Event)>
 where
     F: Fn(Input, Event) -> (Output, Answer),
-    Input: Vertex,
-    Output: Vertex,
+    Input: Any,
+    Output: Any,
 {
     fn from(f: F) -> Self {
         Self(f, PhantomData)
@@ -78,15 +108,15 @@ where
 impl<F, Input, Output, Answer, Event> Transition<Event> for FuncTransition<F, (Input, Event)>
 where
     F: Fn(Input, Event) -> (Output, Answer),
-    Input: Vertex,
-    Output: Vertex,
+    Input: Any,
+    Output: Any,
 {
     type Answer = Answer;
 
-    fn transition(&self, from: Box<dyn Vertex>, event: Event) -> Result<(Box<dyn Vertex>, Answer), TransitionError<Event>> {
-        // TODO: remove Any::is check because it must be done by caller.
+    fn transition(&self, from: &mut dyn Vertex, event: Event) -> Result<(Box<dyn Any>, Answer), TransitionError<Event>> {
         from.exit();
-        let input = from.data().downcast::<Input>()
+        // TODO: remove Any::is check because it must be done by caller.
+        let input = from.get_data().downcast::<Input>()
             .unwrap_or_else(|_| panic!("It is caller task"));
         let out = (self.0)(*input, event);
         Ok((Box::new(out.0), out.1))
@@ -102,47 +132,49 @@ where
 }
 
 pub struct StateMachine<Event, Answer> {
-    state: Option<Box<dyn Vertex>>,
+    state: usize,
     vertexes: Vec<Box<dyn Vertex>>,
     transitions: HashMap<TypeId, Vec<Box<dyn Transition<Event, Answer = Answer>>>>,
 }
 
 impl<Event, Answer> StateMachine<Event, Answer> {
     pub fn new() -> Self {
-        let state = Some(InitialPseudostate::boxed());
-        let vertexes = vec![InitialPseudostate::boxed()];
+        let vertexes = vec![Box::new(State::new(InitialPseudostate)) as _];
         let transitions = HashMap::new();
-        StateMachine { state, vertexes, transitions }
+        StateMachine { state: 0, vertexes, transitions }
     }
     pub fn register_vertex(mut self, vertex: Box<dyn Vertex>) -> Self {
         self.vertexes.push(vertex);
         self
     }
     pub fn transition<T: Transition<Event, Answer = Answer> + 'static>(mut self, transition: T) -> Self {
-        let trans = Box::new(transition);
+        assert!(self.find_vertex_by_data_tid(transition.input_tid()).is_some(), "Not found input vertex!");
+        assert!(self.find_vertex_by_data_tid(transition.output_tid()).is_some(), "Not found output vertex!");
 
+        let trans = Box::new(transition);
         self.transitions.entry(trans.input_tid())
             .or_default()
             .push(trans);
         self
     }
     pub fn process(&mut self, mut event: Event) -> Result<Answer, StateMachineError> {
-        let mut state = self.state.take().expect("It should be Some()");
+        let state = self.vertexes[self.state].as_mut();
         let state_tid = state.data_tid();
 
         let transitions = self.transitions.get(&state_tid).ok_or(StateMachineError::NoTransition)?;
         for transition in transitions {
             match transition.transition(state, event) {
                 Ok((new_state, answer)) => {
-                    new_state.entry();
-                    self.state = Some(new_state);
+                    let new_vertex = self.find_vertex_by_data_tid(new_state.as_ref().type_id()).expect("It should be checked in the `transition` function");
+                    self.vertexes[new_vertex].set_data(new_state);
+                    self.vertexes[new_vertex].entry();
+                    self.state = new_vertex;
                     return Ok(answer);
                 }
                 Err(e) => {
-                    let TransitionError { from: from1, event: event1, kind } = e;
+                    let TransitionError { event: event1, kind } = e;
                     match kind {
                         TransitionErrorKind::GuardErr => {
-                            state = from1;
                             event = event1;
                             continue
                         },
@@ -152,6 +184,9 @@ impl<Event, Answer> StateMachine<Event, Answer> {
         }
 
         Err(StateMachineError::NoTransition)
+    }
+    fn find_vertex_by_data_tid(&self, tid: TypeId) -> Option<usize> {
+        self.vertexes.iter().enumerate().find(|(_, x)| x.data_tid() == tid).map(|(x, _)| x)
     }
 }
 
@@ -169,8 +204,9 @@ mod tests {
         struct SomeState;
 
         let mut machine = StateMachine::new()
+            .register_vertex(State::empty::<SomeState>().boxed())
             .transition(ftrans(|_: InitialPseudostate, event: i32| {
-                (State(SomeState), event * 2)
+                (SomeState, event * 2)
             }));
 
         assert_eq!(machine.process(3), Ok(6));
