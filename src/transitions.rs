cd1e1af7c5d550179b72055a72156f48f3aa9c03
p0lunin <dmytro.polunin@gmail.com>
2022-04-18 14:23:03 +0300

    Add struct variant for transactions.

--- src/transitions.rs
+++ src/transitions.rs
@@ -94,6 +94,44 @@ macro_rules! transitions {
         }
     };
 
+    // ------------IMPL TRANSITION STRUCT STATE------------
+    (
+        @inner
+        event : $event:ident;
+        match $state:ident {
+            $state_v:ident { $($spt:tt)* } => match event {
+                $($t:tt)*
+            }
+            $($tokens1:tt)*
+        }
+        [$($idents:ident),*]
+    ) => {
+        impl $crate::Transition<$event> for $state_v {
+            type State = $state;
+            #[allow(unreachable_patterns)]
+            fn transition(self, event: $event) -> Result<$state, $crate::TransitionError<$state, $event>> {
+                let $state_v { $($spt)* } = self;
+                $crate::transitions! {
+                    @event_matching
+                    $event,
+                    $state::$state_v($state_v { $($spt)* }),
+                    match event {
+                        $($t)*
+                    }
+                }
+            }
+        }
+
+        $crate::transitions! {
+            @inner
+            event : $event;
+            match $state {
+                $($tokens1)*
+            }
+            [$($idents,)* $state_v]
+        }
+    };
+
     // ------------IMPL TRANSITION ENUM------------
     (
         @inner
@@ -156,6 +194,29 @@ macro_rules! transitions {
     ) => {
         match $event_id {
             $event::$pe( $pe( $($fpats)* ) ) => Ok($out.into()),
+            _ => $crate::transitions! {
+                @event_matching
+                $event,
+                $this,
+                match $event_id {
+                    $($t)*
+                }
+            }
+        }
+    };
+
+    // ------------IMPL MATCHING STRUCT EVENT------------
+    (
+        @event_matching
+        $event:ident,
+        $this:expr,
+        match $event_id:ident {
+            $pe:ident { $($fpats:tt)* } => $out:expr;
+            $($t:tt)*
+        }
+    ) => {
+        match $event_id {
+            $event::$pe( $pe { $($fpats)* } ) => Ok($out.into()),
             _ => $crate::transitions! {
                 @event_matching
                 $event,
@@ -294,6 +355,56 @@ mod compile_tests {
 
             let err = state3.transition(Event2);
             assert_eq!(err, Err(TransitionError::NoTransition(State3("test".into()).into(), Event2.into())));
+        }
+    }
+
+    mod test3 {
+        use crate::{stenum, TransitionError};
+
+        stenum! {
+            #[derive(Debug, PartialEq)]
+            enum Event {
+                struct Event1 { str: String }
+                struct Event2;
+            }
+        }
+
+        stenum! {
+            #[derive(Debug, PartialEq)]
+            enum State {
+                struct State1;
+                struct State2 { str: String }
+                struct State3 { str: String }
+            }
+        }
+
+        transitions! {
+            event : Event;
+
+            match State {
+                State1 => match event {
+                    Event1 { str } => State2 { str };
+                    Event2 => State3 { str: String::new() };
+                }
+                State2 { str } => match event {
+                    Event2 => State3 { str };
+                }
+                State3 => match event {}
+            }
+        }
+
+        #[test]
+        fn test_transitions_2() {
+            let state1: State = State1.into();
+
+            let state2 = state1.transition(Event1 { str: "test".into() }).unwrap();
+            assert_eq!(state2, State2 { str: "test".into() }.into());
+
+            let state3 = state2.transition(Event2).unwrap();
+            assert_eq!(state3, State3 { str: "test".into() }.into());
+
+            let err = state3.transition(Event2);
+            assert_eq!(err, Err(TransitionError::NoTransition(State3 { str: "test".into() }.into(), Event2.into())));
         }
     }
 
